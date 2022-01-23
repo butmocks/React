6390c24cf0b49e11d1990b2ebe1db610fd5ca546
Yaroslav Platonov <butmocks@gmail.com>
2022-01-23 17:10:57 +0200

    444

--- 1intro/homework/todo-list/src/index.js
+++ 1intro/homework/todo-list/src/index.js
@@ -7,9 +7,7 @@ import './index.css';
 // <h1 class="title">Todo List</h1>
 const rootElement = document.querySelector('#root');
 
-const element = (
-  <h1 className="title">Todo List</h1>
-);
+const element = <h1 className="title">Todo List</h1>
 
 
 
