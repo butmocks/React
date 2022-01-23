d136a7f38463395513ab9ad4399cc51ecf2666e2
Yaroslav Platonov <butmocks@gmail.com>
2022-01-23 02:40:53 +0200

    2

--- 1intro/homework/hometask2/index.js
+++ 1intro/homework/hometask2/index.js
@@ -1,20 +1,37 @@
 const rootElement = document.querySelector('#root');
 
-const renderGreeting = elem => {
-  const containerElem = document.createElement('div');
-  containerElem.classList.add('greeting');
+// const renderGreeting = elem => {
+//   const containerElem = document.createElement('div');
+//   containerElem.classList.add('greeting');
 
-  const titleElem = document.createElement('h1');
-  titleElem.classList.add('greeting__title');
-  titleElem.textContent = 'Hello, world!';
+//   const titleElem = document.createElement('h1');
+//   titleElem.classList.add('greeting__title');
+//   titleElem.textContent = 'Hello, world!';
 
-  const textElem = document.createElement('p');
-  textElem.classList.add('greeting__text');
-  textElem.textContent = "I'm learning React";
+//   const textElem = document.createElement('p');
+//   textElem.classList.add('greeting__text');
+//   textElem.textContent = "I'm learning React";
 
-  containerElem.append(titleElem, textElem);
+//   containerElem.append(titleElem, textElem);
 
-  elem.append(containerElem);
-};
+//   elem.append(containerElem);
+// };
 
-renderGreeting(rootElement);
+// renderGreeting(rootElement);
+
+const element = React.createElement(
+  'div',
+  { className: 'greeting' },
+    React.createElement(
+      'div',
+      { className: 'greeting__title' },
+      'Hello world!'
+    ),
+    React.createElement(
+      'div',
+      { className: 'greeting__text' },
+      'I\'m learning React'
+    ),
+);
+
+ReactDOM.render(element, rootElement);
