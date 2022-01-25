791c058036b04efabe69c923ce4904e2d9f824b4
Yaroslav Platonov <butmocks@gmail.com>
2022-01-25 21:30:16 +0200

    3.1

---
+++ 3components/task3/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Comment from './Comment';
+
+const user = {
+  name: 'Tom',
+  аvatarUrl: 'https://avatars1.githubusercontent.com',
+};
+
+const App = () => {
+  return <Comment author={user} text="Good job!" date={new Date('2019-01-01T11:32:19.566Z')} />;
+};
+
+export default App;
---
+++ 3components/task3/src/Avatar.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Avatar(props) {
+  return <img className="avatar" src={props.avatarUrl} alt={props.name} />;
+}
+export default Avatar;
