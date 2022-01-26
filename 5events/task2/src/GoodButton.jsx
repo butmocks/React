a14234a1091a1fd9a326cd983106f1094c808d8c
butmocks <butmocks@gmail.com>
2022-01-26 23:09:40 +0200

    5.1

--- 5events/task2/src/GoodButton.jsx
+++ 5events/task2/src/GoodButton.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 class GoodButton extends React.Component {
-  handleClick(e) {
-    console.log(e.target.textContent);
+  handleClick() {
     alert('Good job!');
   }
 
