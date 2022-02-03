a8219c9195a910882489223dc447ed5f4eba02d7
butmocks <butmocks@gmail.com>
2022-02-03 13:46:47 +0200

    asdfgh

--- 7lists/homework/hometask1/src/index.jsx
+++ 7lists/homework/hometask1/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import TransactionsList from './TransactionsList';
+import TransactionsList from './TransactionsList.jsx';
 
 const rootElement = document.querySelector('#root');
 const transactions = [
