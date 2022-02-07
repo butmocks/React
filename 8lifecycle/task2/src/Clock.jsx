c540481d98c05a36e85ecbedcf0da58d304d719c
butmocks <butmocks@gmail.com>
2022-02-07 15:40:38 +0200

    cf

---
+++ 8lifecycle/task2/src/Clock.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Clock extends Component {
+  constructor() {
+    this.state = {
+      date: new Date()
+    }:
+  }
+  render() {
+    return null;
+  }
+}
+
+export default Clock;
