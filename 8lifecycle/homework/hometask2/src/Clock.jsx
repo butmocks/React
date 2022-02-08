b98a2b2f3f77a853ebf5c7e9527dd693dde7f71d
butmocks <butmocks@gmail.com>
2022-02-08 18:41:22 +0200

    f

---
+++ 8lifecycle/homework/hometask2/src/Clock.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './index.scss';
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: this.getTimeWithOffset(this.props.offset),
+    };
+  }
+  getTimeWithOffset = offset => {
+    const currentTime = new Date();
+
+    const utcOffset = currentTime.getTimezoneOffset() / 60;
+    return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        date: this.getTimeWithOffset(this.props.offset),
+      });
+    }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        <div className="clock__location">{this.props.location}</div>
+        <div className="clock__time">
+          {this.state.date.toLocaleTimeString()}
+          </div>
+      </div>
+    );
+  }
+}
+
+export default Clock;
