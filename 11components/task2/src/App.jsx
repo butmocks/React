73a35bf004bb3e39595441f2cb39f5b39d949c8e
butmocks <butmocks@gmail.com>
2022-02-14 07:31:16 +0200

    11

---
+++ 11components/task2/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+
+
+  render() {
+   
+
+    return (
+      <div className="p>
+
+        
+      </div>
+    );
+  }
+  }
+  
+
+export default App;
